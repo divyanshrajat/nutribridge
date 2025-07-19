37127fe403c5572d05ac03aa3f955ae441517445
DIVYANSH RAJAT <divyanshrajat1999@gmail.com>
2025-07-19 22:56:40 +0530

    Delete src directory

--- src/assets/icons/index.js
+++
@@ -1,4 +0,0 @@
-// Central export for all icons
-export { default as Logo } from './Logo';
-export { default as HealthIcon } from './HealthIcon';
-export { default as VoiceIcon } from './VoiceIcon';
--- src/assets/index.js
+++
@@ -1,3 +0,0 @@
-// Central export for all assets
-export * as flags from './flags';
-export * as icons from './icons';
